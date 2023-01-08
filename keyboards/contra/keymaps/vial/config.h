4698fb513d34094ccf302630a9d0fcd8631dd918
Ben Clarke <ben@arrayofstars.com>
2023-01-08 09:30:41 -0500

    Add support for Cartel Contra

--- keyboards/contra/keymaps/vial/config.h
+++ keyboards/contra/keymaps/vial/config.h
@@ -1,6 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0xD7, 0x90, 0x14, 0xEE, 0x46, 0xFC, 0xC7, 0x22}
-
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define VIAL_KEYBOARD_UID {0xE7, 0xD1, 0xB6, 0xA3, 0x57, 0x2A, 0xF4, 0xFC}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 } 
