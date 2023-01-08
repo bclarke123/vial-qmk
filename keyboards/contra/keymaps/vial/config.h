77c03a4a822040764748e831f47434c5b75efade
Ben Clarke <ben@arrayofstars.com>
2023-01-08 09:45:34 -0500

    Dynamic keymaps aren't going to work

--- keyboards/contra/keymaps/vial/config.h
+++ keyboards/contra/keymaps/vial/config.h
@@ -2,7 +2,6 @@
 
 #pragma once
 
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define VIAL_KEYBOARD_UID {0xE7, 0xD1, 0xB6, 0xA3, 0x57, 0x2A, 0xF4, 0xFC}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 11 } 
